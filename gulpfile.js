555d8d36e38be5fcedb6811447509ad573aabccb
Tomo Masakura <tomo.masakura@gmail.com>
2015-12-05 12:31:03 +0900

    switch test local app

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,25 @@
 const gulp = require('gulp');
 const gutil = require('gulp-util');
+const connect = require('gulp-connect');
+const protractor = require('gulp-protractor').protractor;
 
 gulp.task('default', function taskDefault() {
   gutil.log('Finish log');
 });
 
-gulp.task('test', function taskTest() {
-  gutil.log('Success');
+gulp.task('serve:test', function taskServeTest() {
+  connect.server({
+    port: 8001,
+    root: 'app',
+  });
+});
+
+gulp.task('protractor', ['serve:test'], function taskProtractor() {
+  return gulp.src('./spec/**/*.spec.js')
+    .pipe(protractor())
+    .on('error', e => {throw e;});
+});
+
+gulp.task('test', ['protractor'], function taskTest() {
+  connect.serverClose();
 });
--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,7 +1,11 @@
 describe('test1', function test1() {
-  it('test11', function test2() {
-    browser.get('https://angular.github.io/protractor/#/');
+  beforeEach(function setup() {
+    browser.ignoreSynchronization = true;
+  });
 
-    expect(browser.getTitle()).toEqual('Protractor - end to end testing for AngularJS');
+  it('test11', function test2() {
+    browser.get('http://localhost:8001');
+
+    expect(browser.getTitle()).toEqual('Bootstrap take screenshot');
   });
 });
