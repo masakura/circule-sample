341535f614a3153476c24da8566357689cdcd887
Tomo Masakura <tomo.masakura@gmail.com>
2015-12-05 12:46:18 +0900

    add test report file

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,7 +1,16 @@
+const JUnitXmlReporter = require('jasmine-reporters').JUnitXmlReporter;
+
 exports.config = {
   directConnect: true,
   capabilities: {
     browserName: 'chrome',
   },
   specs: ['spec/**/*.spec.js'],
+
+  onPrepare: () => {
+    jasmine.getEnv()
+      .addReporter(new JUnitXmlReporter({
+        savePath: 'reports/tests',
+      }));
+  },
 };
