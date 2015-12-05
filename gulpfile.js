d4e5ae538d58d03f70e18646248b4cb75a2bd73c
Tomo Masakura <tomo.masakura@gmail.com>
2015-12-05 14:37:33 +0900

    Add bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,30 @@ const gutil = require('gulp-util');
 const connect = require('gulp-connect');
 const protractor = require('gulp-protractor').protractor;
 const rimraf = require('rimraf');
+const wiredep = require('wiredep').stream;
 
 gulp.task('default', function taskDefault() {
   gutil.log('Finish log');
+});
+
+gulp.task('wiredep', function taskWiredep() {
+  return gulp.src('app/index.html')
+    .pipe(wiredep())
+    .pipe(gulp.dest('app', { overwrite: true }));
 });
 
 gulp.task('clean:test', function taskCleanTest() {
   rimraf('reports/tests', () => {});
 });
 
-gulp.task('serve:test', function taskServeTest() {
+gulp.task('serve:test', ['wiredep'], function taskServeTest() {
   connect.server({
     port: 8001,
-    root: 'app',
+    root: [
+      '.tmp',
+      '.',
+      'app',
+    ],
   });
 });
 
