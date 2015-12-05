06547aed041a3d19e5cd32c552258b54192feddb
Tomo Masakura <tomo.masakura@gmail.com>
2015-12-05 14:46:31 +0900

    remove assertion

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -5,7 +5,5 @@ describe('test1', function test1() {
 
   it('test11', function test2() {
     browser.get('http://localhost:8001');
-
-    expect(browser.getTitle()).toEqual('Bootstrap take screenshot');
   });
 });
