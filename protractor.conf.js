d11dba498a16cc855a501483c0551503adc52e58
Tomo Masakura <tomo.masakura@gmail.com>
2015-12-05 14:02:31 +0900

    fix cannot take screenshot with firefox

--- protractor.conf.js
+++ protractor.conf.js
@@ -3,15 +3,20 @@ const JUnitXmlReporter = require('jasmine-reporters').JUnitXmlReporter;
 const HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
 
 exports.config = {
-  directConnect: true,
   capabilities: {
-    browserName: 'chrome',
+    browserName: 'firefox',
   },
   specs: ['spec/**/*.spec.js'],
 
   onPrepare: () => {
+    var info = {};
+
     // https://github.com/mlison/protractor-jasmine2-screenshot-reporter/issues/4#issuecomment-146556304
-    browser.getCapabilities().then(c => browser.capabilities = c);
+    browser.getCapabilities().then(c => {
+      info.base = path.join(c.get('platform'),
+                            c.get('browserName'),
+                            c.get('version'));
+    });
 
     jasmine.getEnv()
       .addReporter(new JUnitXmlReporter({
@@ -21,12 +26,9 @@ exports.config = {
       .addReporter(new HtmlScreenshotReporter({
         dest: 'reports/tests/screenshot',
         filename: 'index.html',
-        pathBuilder: (spec) => {
-          const c = browser.capabilities;
-          return path.join(c.get('platform'),
-                           c.get('browserName'),
-                           c.get('version'),
-                           spec.fullName);
+        pathBuilder: spec => {
+          console.log(spec);
+          return path.join(info.base, spec.fullName);
         },
       }));
   },
